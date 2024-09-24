568304a18eaf4acf8deb810279ee5c80f51eef71
ThaTechMaestro <muse.pearls@gmail.com>
2024-09-24 20:50:10 +0100

    check in: navigating api flow

---
+++ src/ell/providers/openai-test.py
@@ -0,0 +1,9 @@
+# Test Implementing an open ai provider using the base provider class
+# compare implementation with openai.py
+# What are we trying to achieve?
+# Last Question to think about -- what is the goal of this API implementation?
+
+from ell.provider import Provider
+
+class Openai(Provider):
+    pass
